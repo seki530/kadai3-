b8fa37eb7bd1979f568c79d3fae7ed05497a4d9f
EC2 Default User <ec2-user@ip-172-31-23-167.ap-northeast-1.compute.internal>
2023-06-07 12:15:31 +0000

    booksのedit,show,index,destroyの追加

---
+++ app/models/book.rb
@@ -0,0 +1,21 @@
+class Book < ApplicationRecord
+  has_one_attached :profile_image
+  belongs_to :user
+
+  def get_profile_image
+    if profile_image.attached?
+      profile_image
+    else
+      'no_image.jpg'
+    end
+  end
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+end
